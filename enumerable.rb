8a039109879d85ebe32b3ca68a8ca4ed73b0b64d
kennankole <kennankole@gmail.com>
2023-01-27 08:28:44 +0300

    Moved MyEnumerable module to a seperate file.

--- enumerable.rb
+++ enumerable.rb
@@ -1,26 +1,4 @@
-module MyEnumerable
-  def all?
-    (0...@list.length).each do |i|
-      return false unless yield(@list[i])
-    end
-    true
-  end
-
-  def any?
-    (0...@list.length).each do |i|
-      return true if yield(@list[i])
-    end
-    false
-  end
-
-  def filter
-    results = []
-    (0...@list.length).each do |i|
-      results.push(@list[i]) if yield(@list[i])
-    end
-    results
-  end
-end
+require './my_enumerable'
 
 class MyList
   include MyEnumerable
